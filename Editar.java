c5a9691548d2c2bec31ffcbc1d9d7015ad54bc19
Gabriel Pereira <gabrielpereira0907@gmail.com>
2023-12-22 17:04:35 -0300

    Modificação no codigo Editar.

--- Editar.java
+++ Editar.java
@@ -8,7 +8,7 @@ public class Editar {
     
     private int indice_pessoas;
     private int opcao_edit;
-    private int sair = 0;
+    
     
     public void Edicao(){
         System.out.println("Qual é o número do seu cadastro? digite de 1 a " + edit.num_pessoas);
@@ -18,8 +18,23 @@ public class Editar {
             System.out.println("Número invalido!");
             
         }else{
-            while(sair != 0){
+            while(opcao_edit == 0){
+                System.out.println("Qual dos dados você deseja editar!");
+                System.out.println("");
                 
+                System.out.println("Digite 1 para Nome.");
+                System.out.println("Digite 2 para Idade.");
+                System.out.println("Digite 3 para Telefone.");
+                System.out.println("Digite 4 para CPF.");
+                System.out.println("Digite 5 para Email.");
+                System.out.println("Digite 6 para Estado civil.");
+                System.out.println("Digite 7 para CEP.");
+                System.out.println("Digite 8 para Endereço.");
+                System.out.println("Digite 9 para Número da casa");
+                System.out.println("Digite 10 para Nacionalidade");
+                System.out.println("Digite 0 para sair da edição.");
+                System.out.print("Digite aqui: ");
+                this.opcao_edit = edit2.nextInt();
             }
         }
         
